7705c427b42fad12ea78006dc9a8c67afba10b7c
Alefsandler Felisberto <aleffelisberto@gmail.com>
2022-07-15 08:09:24 -0300

    feat: add App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import { WeatherContent } from "./components/WeatherContent"
+
 export function App() {
   return (
-    <div className="App">
-      <h1 className="">Hello World</h1>
-    </div>
+    <WeatherContent />
   )
 }
